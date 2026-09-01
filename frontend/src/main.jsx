429a874a95f501dddd89d94a3d30f089a59cd4e5
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-09-01 19:34:12 +0530

    frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import AuthProvider from "./context/AuthProvider";
-
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-
-    </StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
