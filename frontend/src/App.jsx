4f5b6bd9699874719dc10e787f9c800664c8d6a4
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-09-16 19:45:41 +0530

    app jsx name fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import useAuth from "./context/useAuth";
 import AllTransactions from "./pages/AllTransactions";
 import Profile from "./pages/Profile";
-import BankTransactions from "./pages/bankTransactions";
+import BankTransactions from "./pages/BankTransactions";
 
 function PublicRoute({ children }) {
     const { user, isLoading } = useAuth();
